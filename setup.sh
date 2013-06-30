9525b0618fb10a3ff5960ea7e7f047fffe6f0291
Phil Hagelberg <technomancy@gmail>
2013-06-29 19:58:34 -0700

    Bring in festival.

--- setup.sh
+++ setup.sh
@@ -11,7 +11,11 @@ echo "APT::Install-Recommends \"0\";" > /etc/apt/apt.conf.d/50norecommends
 
 apt-get update
 apt-get install -y avahi-daemon tmux build-essential navit mpd mpc \
-    libdbus-1-dev libncurses5-dev
+    festival festvox-kallpc16k libdbus-1-dev libncurses5-dev
+
+# TODO: british voice
+# http://festvox.org/databases/cstr_uk_rab_diphone/packed/cstr_uk_rab_diphone.tar.bz2
+# into /usr/share/festival/voices/english
 
 # Shaves 10s off boot time
 update-rc.d -f rsyslog remove
@@ -35,14 +39,14 @@ sed -i s_/sbin/getty\ --noclear_/sbin/getty\ --autologin\ pi_ /etc/inittab
 # install Jarvis
 
 HOME=/home/pi
-mkdir $HOME/.emacs.d
+mkdir -p $HOME/.emacs.d
 ln -s $(dirname $0)/init.el $HOME/.emacs.d
 ln -s $(dirname $0)/jarvis.el $HOME/.emacs.d
 ln -s $(dirname $0)/tmux.conf $HOME/.tmux.conf
 ln -s $(dirname $0)/bash_profile $HOME/.bash_profile
 
 # music
-mkdir $HOME/music
+mkdir -p $HOME/music
 sed -i s_/var/lib/mpd/music_/home/pi/music_ /etc/mpd.conf
 /etc/init.d/mpd restart
 
